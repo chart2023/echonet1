48e719ee193da1a624dd8cdf87ae956b6dd4e28b
Apichart <chart2023@users.noreply.github.com>
2017-03-04 00:48:27 +0700

    Update install_echonet.sh

--- install_echonet.sh
+++ install_echonet.sh
@@ -3,6 +3,12 @@ echo "STEP1: INSTANTIATE echonet gw"
 echo "START at:" $(date)
 NSCL_START_TIME=$(date)
 myhome=${HOME}
+route add -net 192.168.9.0/24 gw 10.0.14.1
+route add -net 10.0.14.0/24 gw 10.0.14.1
+route add -net 161.200.90.0/24 gw 10.0.14.1
+route add -net 91.189.88.0/24 gw 10.0.14.1
+route add default gw 192.168.0.1
+route del default gw 10.0.14.1
 dpkg-reconfigure -f noninteractive tzdata
 apt-get update
 apt-get install --reinstall tzdata -y
@@ -15,12 +21,6 @@ else
         echo "Server cannot connect to Internet"
 fi
 ifconfig ens3 $private_echonet netmask 255.255.255.0
-route add -net 192.168.9.0/24 gw 10.0.14.1
-route add -net 10.0.14.0/24 gw 10.0.14.1
-route add -net 161.200.90.0/24 gw 10.0.14.1
-route add -net 91.189.88.0/24 gw 10.0.14.1
-route add default gw 192.168.0.1
-route del default gw 10.0.14.1
 node /home/ubuntu/echonetlite/findechonet.js
 echo "exports.TempUpper='$TempUpper';" | tee --append /home/ubuntu/echonetlite/device.info
 echo "exports.TempLower='$TempLower';" | tee --append /home/ubuntu/echonetlite/device.info
