4863686123c027c4df70226ed73a9bba0ebed0f6
Apichart <chart2023@users.noreply.github.com>
2017-03-10 16:20:01 +0700

    Update server_configure.sh

--- server_configure.sh
+++ server_configure.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "configure"
 echo "START at:" $(date)
+echo "*/10 * * * * /usr/bin/nodejs /home/ubuntu/echonetlite/echonetmain.js >> /home/ubuntu/echonetlite/airresult.log" | tee --append /etc/cron.d/echonetlite
 echo "FINISH at:" $(date)
 echo "##########FINISHED############"
