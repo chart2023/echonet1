d41506ee6fc655e9ce7ec98c9fb56e18e86b6af3
Apichart <chart2023@users.noreply.github.com>
2017-03-10 16:39:05 +0700

    Update start_echonet.sh

--- start_echonet.sh
+++ start_echonet.sh
@@ -2,5 +2,6 @@
 echo "START"
 echo "START at:" $(date)
 /usr/bin/node /home/ubuntu/echonetlite/findechonet.js
+/usr/bin/node /home/ubuntu/echonetlite/echonetmain.js
 echo "FINISH at:" $(date)
 echo "##########FINISHED############"
